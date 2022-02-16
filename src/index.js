e01e5053b9d69b16752c23ba23f77add0cac934a
Marah Rice <91274269+MarahDeven@users.noreply.github.com>
2022-02-16 09:04:58 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React AJAX</h1>
+
+      <h3>Challenge 1</h3>
+      <p>Import axios</p>
+
+      <h3>Challenge 2</h3>
+      <p>
+        Create a new component called Weather with a city property which returns
+        an h1 with the sentence "The temperature in city is something°C".
+        Include it here
+      </p>
+
+      <h3>Challenge 3</h3>
+      <p>Use the OpenWeather API to get the city temperature in Celsius</p>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
