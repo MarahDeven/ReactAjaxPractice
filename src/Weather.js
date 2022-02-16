740050340bcb4814956d5e6be0e311e761e6ea1b
Marah Rice <91274269+MarahDeven@users.noreply.github.com>
2022-02-16 09:26:20 -0500

    Used the OpenWeather API to get the city temperature in Fahrenheit

--- src/Weather.js
+++ src/Weather.js
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function Weather(props) {
-  let [temperature, setTemperatre] = useState(null);
+  let [temperature, setTemperature] = useState(null);
   function displayTemperature(response) {
-    console.log(response.data);
+    setTemperature(response.data.main.temp);
   }
 
   let apiKey = "4af869c2b16527dd3b375c7f352dada0";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
   axios.get(url).then(displayTemperature);
 
-  return <h1>The weather in {props.city} is 26°C</h1>;
+  if (temperature) {
+    return (
+      <h1>
+        The weather in {props.city} is {Math.round(temperature)}°F
+      </h1>
+    );
+  } else {
+    return <p>Loading...</p>;
+  }
 }
